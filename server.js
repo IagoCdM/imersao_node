ac21a21febee0cb680b4bd0a06227156443f4eaa
IagoCdM <iagocarneirosc@gmail.com>
2026-02-25 11:29:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ import fastify from 'fastify';
 
 const servidor = fastify();
 
+const usuarios = []
+
 servidor.get('/usuarios', () => {
-    return 'ola mundo!'
+    return usuarios
 }) 
+
 servidor.post('/usuarios', (request, reply) => {
-    return 'ola mundo!'
+    const body = request.body
+    usuarios.push(body)
+    return 'Usuario cadastrado'
 }) 
+
 servidor.listen({
     port: 1584
 })
